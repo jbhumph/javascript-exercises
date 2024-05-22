f1b4c37d7a5c5b82921a86b6867ef3dd08d374b1
John <jbhumph@gmail.com>
2024-05-22 16:50:31 -0700

    Finished exercise 02.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(word, number) {
+    cat = ''
+    if (number < 0) {
+        cat = 'ERROR'
+    }
+    for (i = 0; i < number; i++) {
+        cat += word
+    }
 
+    return cat
 };
 
 // Do not edit below this line
