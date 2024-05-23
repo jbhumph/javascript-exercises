48434fd1662b47df5d0a79efd3dad7c9f03631e8
John <jbhumph@gmail.com>
2024-05-22 18:35:29 -0700

    Finished exercise 04.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, val1, val2, val3, val4) {
+    newArray = [];
+    j = 0;
+    for (i = 0; i < array.length; i++) {
+        if (array[i] != val1 && array[i] != val2 && array[i] != val3 && array[i] != val4) {
+            newArray[j] = array[i];
+            j++;
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
