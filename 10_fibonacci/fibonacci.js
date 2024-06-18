92ea524f785d8c9ba2e43ee281057f9f712a0b9b
John <jbhumph@gmail.com>
2024-06-18 11:47:09 -0700

    Finished 10 fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(y) {
+    x = parseInt(y);
+    if (x < 0) return "OOPS"
+    let fibb = [0, 1];
+    while (fibb.length < x+1) {
+        fibb.push(fibb[fibb.length-1] + fibb[fibb.length-2]);
+    }
+    return fibb[x]
 };
 
 // Do not edit below this line
